c5d9c7059ed8df204c13ba11c2ab7903a298c988
Ameer Jhan <ameerjhanprof@gmail.com>
2021-02-17 13:18:44 +0530

    feat: add routes to app

---
+++ examples/react/src/About.js
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About page working!!</h1>
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
--- examples/react/src/index.js
+++ examples/react/src/index.js
@@ -1,12 +1,21 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, HashRouter, Route } from 'react-router-dom';
+import About from './About';
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Route path="/" exact={true}>
+        <App />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+    </BrowserRouter>
   </StrictMode>,
   rootElement
 );
