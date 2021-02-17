cfdafebeedaeba47c3138eca0da570603a2775f8
Ameer Jhan <ameerjhanprof@gmail.com>
2021-02-17 15:18:34 +0530

    feat: add history handling

--- src/client/index.js
+++ src/client/index.js
@@ -6,6 +6,32 @@ let sandpackManager = new Manager('#root', { showOpenInCodeSandbox: false });
 const info = (message) => console.log(`${name}: ${message}`)
 const ws = new WebSocket(`ws://${window.location.host}`);
 let sandboxFiles;
+let historyStateUID = 0;
+
+function getRelativeUrl(url) {
+  return url.replace(sandpackManager.bundlerURL, "");
+}
+
+window.onpopstate = () => {
+  // back btn
+  if (historyStateUID > history.state.uid) {
+    sandpackManager.dispatch({ type: 'urlback' });
+  } else {
+    sandpackManager.dispatch({ type: 'urlforward' });
+  }
+}
+
+sandpackManager.listen((evt) => {
+  switch (evt.type) {
+    case "urlchange": {
+      const relativeUrl = getRelativeUrl(evt.url);
+
+      history.pushState({ uid: historyStateUID }, '', relativeUrl);
+      historyStateUID++;
+    }
+  }
+});
+
 
 ws.onopen = () => info('connected');
 
