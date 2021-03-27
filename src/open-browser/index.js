d10bed84af8661fa2342413bdccb9038dea452f6
Ameer Jhan <ameerjhanprof@gmail.com>
2021-03-27 16:52:07 +0530

    fix: browser not opening in linux

--- src/open-browser/index.js
+++ src/open-browser/index.js
@@ -1,13 +1,8 @@
 var execSync = require('child_process').execSync;
 var open = require('open');
 
-// https://github.com/sindresorhus/open#app
-var OSX_CHROME = 'google chrome';
-
 function openBrowser(url) {
-  const shouldTryOpenChromiumWithAppleScript =
-    process.platform === 'darwin' &&
-    (typeof browser !== 'string' || browser === OSX_CHROME);
+  const shouldTryOpenChromiumWithAppleScript = process.platform === 'darwin';
 
   if (shouldTryOpenChromiumWithAppleScript) {
     // Will use the first open browser found from list
@@ -46,8 +41,7 @@ function openBrowser(url) {
   // Fallback to open
   // (It will always open new tab)
   try {
-    var options = { app: browser, wait: false, url: true };
-    open(url, options).catch(() => {}); // Prevent `unhandledRejection` error.
+    open(url).catch(() => {}); // Prevent `unhandledRejection` error.
     return true;
   } catch (err) {
     return false;
